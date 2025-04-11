097226559c9342805713a885fedec1749c6a793b
Konrad Falkowski <konrad.falkowski@onewelcome.com>
2025-04-11 12:07:33 +0200

    [EXAMPLEIOS-118] SDK initialisation

--- SDK Showcase/SDK Showcase/Helpers/Binding.swift
+++ SDK Showcase/SDK Showcase/Helpers/Binding.swift
@@ -11,4 +11,9 @@ extension Binding {
         self.init(get: { source.wrappedValue == nil ? defaultValue as? Value ?? Bool() : source.wrappedValue as? Value ?? Bool() },
                   set: { source.wrappedValue = ($0 as! T)})
     }
+    
+    init<T>(isNotNil source: Binding<T?>, defaultValue: T?) where Value == Double {
+        self.init(get: { source.wrappedValue == nil ? defaultValue as? Value ?? Double() : source.wrappedValue as? Value ?? Double() },
+                  set: { source.wrappedValue = ($0 as! T)})
+    }
 }
--- SDK Showcase/SDK Showcase/Views/Categories/ActionView.swift
+++ SDK Showcase/SDK Showcase/Views/Categories/ActionView.swift
@@ -16,7 +16,12 @@ struct ActionView: View {
             HStack {
                 if (action.providedValue is Bool || action.defaultValue is Bool) {
                     Toggle(action.name, isOn: Binding(isNotNil: $action.providedValue, defaultValue: action.defaultValue))
-                } else {
+                }
+                else if (action.providedValue is Double || action.defaultValue is Double) {
+                    Slider(value: Binding(isNotNil: $action.providedValue, defaultValue: action.defaultValue), in: 0...100, step: 1.0)
+                    Text("\((action.providedValue ?? action.defaultValue) as! Double, specifier: "%.0f")")
+                }
+                else {
                     TextField(text: Binding(isNotNil: $action.providedValue, defaultValue: nil)) {
                         Text(action.defaultValue as? String ?? "")
                     }
